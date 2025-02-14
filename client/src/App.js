bab3f28943af337dcdc61c2b6ca3bfb27b18b6ea
Nico in Space <53238599+Nickalas-Paul@users.noreply.github.com>
2025-02-14 18:40:27 +0000

    Updated Google Geocoding API with Netlify domain

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BirthInputForm from "./BirthInputForm";
 
-function App() {
+const App = () => {
+  const [chartData, setChartData] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AstroAudio App</h1>
+      <BirthInputForm onChartGenerated={setChartData} />
+      {chartData && (
+        <div>
+          <h2>Your Birth Chart Data:</h2>
+          <pre>{JSON.stringify(chartData, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
